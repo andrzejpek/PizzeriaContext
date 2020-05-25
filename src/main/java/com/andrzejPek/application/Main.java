eb1e81f730ca3ee09722a726866fdb87023bf5e9
pekalaandrzej50@gmail.com <pekalaandrzej50@gmail.com>
2020-05-25 11:27:17 +0200

    add class

--- src/main/java/com/andrzejPek/application/Main.java
+++ src/main/java/com/andrzejPek/application/Main.java
@@ -1,7 +1,24 @@
 package com.andrzejPek.application;
 
+import com.andrzejPek.templates.Additives;
+import com.andrzejPek.templates.Size;
+
 public class Main {
     public static void main(String[] args) {
 
+    showAdditives();
+    showSize();
+    }
+
+    public static void showAdditives(){
+        for (Additives d : Additives.values()){
+            System.out.printf("Additives is %s and cost %d %n",d,d.getPrice());
+        }
+    }
+
+    public static void showSize(){
+        for (Size s : Size.values()){
+            System.out.printf("size pizza %s is %d cm %n",s,s.getSize());
+        }
     }
 }
